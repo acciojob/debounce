9220418d6848446817c064b851f45c5efd848240
HarshKhandelwal101 <78348500+HarshKhandelwal101@users.noreply.github.com>
2023-03-06 09:37:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+function debounce(callback, delay, immediate = false) {
+  // Write your code here.
+}
+
+// Do not edit the line below.
+exports.debounce = debounce;
